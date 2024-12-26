6bf73ea052a122bdf679c9f378845683e87860de
xzy <xzy20030705@163.com>
2024-12-26 22:14:37 +0800

    做了简单的购物界面，十个物品分为五个类别，加入购物车算一次cookie，下单也会算一次，下单结算还没做完

--- src/main/java/xzy/shop/HelloServlet.java
+++ src/main/java/xzy/shop/HelloServlet.java
@@ -8,7 +8,10 @@ import jakarta.servlet.annotation.*;
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
+    }
+
+    public void destroy() {
     }
 }
