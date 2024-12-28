e898a428e1b8b97bec3e1fe5bbd9274dbc41bd69
tangandrea <74780221+tangandrea@users.noreply.github.com>
2024-12-29 05:33:41 +0800

    Add files via upload

---
+++ service/GuanggaokehuService.java
@@ -0,0 +1,33 @@
+package com.service;
+
+import com.baomidou.mybatisplus.mapper.Wrapper;
+import com.baomidou.mybatisplus.service.IService;
+import com.utils.PageUtils;
+import com.entity.GuanggaokehuEntity;
+import java.util.List;
+import java.util.Map;
+import com.entity.vo.GuanggaokehuVO;
+import org.apache.ibatis.annotations.Param;
+import com.entity.view.GuanggaokehuView;
+
+
+/**
+ * 广告客户
+ *
+ */
+public interface GuanggaokehuService extends IService<GuanggaokehuEntity> {
+
+    PageUtils queryPage(Map<String, Object> params);
+
+    List<GuanggaokehuVO> selectListVO(Wrapper<GuanggaokehuEntity> wrapper);
+
+    GuanggaokehuVO selectVO(@Param("ew") Wrapper<GuanggaokehuEntity> wrapper);
+
+    List<GuanggaokehuView> selectListView(Wrapper<GuanggaokehuEntity> wrapper);
+
+    GuanggaokehuView selectView(@Param("ew") Wrapper<GuanggaokehuEntity> wrapper);
+
+    PageUtils queryPage(Map<String, Object> params,Wrapper<GuanggaokehuEntity> wrapper);
+
+}
+
---
+++ service/GuanggaoshangService.java
@@ -0,0 +1,34 @@
+package com.service;
+
+import com.baomidou.mybatisplus.mapper.Wrapper;
+import com.baomidou.mybatisplus.service.IService;
+import com.utils.PageUtils;
+import com.entity.GuanggaoshangEntity;
+import java.util.List;
+import java.util.Map;
+import com.entity.vo.GuanggaoshangVO;
+import org.apache.ibatis.annotations.Param;
+import com.entity.view.GuanggaoshangView;
+
+
+/**
+ * 广告商
+ *
+ */
+public interface GuanggaoshangService extends IService<GuanggaoshangEntity> {
+
+    PageUtils queryPage(Map<String, Object> params);
+
+    List<GuanggaoshangVO> selectListVO(Wrapper<GuanggaoshangEntity> wrapper);
+
+    GuanggaoshangVO selectVO(@Param("ew") Wrapper<GuanggaoshangEntity> wrapper);
+
+    List<GuanggaoshangView> selectListView(Wrapper<GuanggaoshangEntity> wrapper);
+
+    GuanggaoshangView selectView(@Param("ew") Wrapper<GuanggaoshangEntity> wrapper);
+
+    PageUtils queryPage(Map<String, Object> params,Wrapper<GuanggaoshangEntity> wrapper);
+
+}
+
+
