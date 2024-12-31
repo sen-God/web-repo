f3b987de5b97bae2a479c90e984f615f4e0cfbc4
tangandrea <74780221+tangandrea@users.noreply.github.com>
2024-12-31 22:34:37 +0800

    Add files via upload

---
+++ service/GuanggaoxuqiuService.java
@@ -0,0 +1,33 @@
+package com.service;
+
+import com.baomidou.mybatisplus.mapper.Wrapper;
+import com.baomidou.mybatisplus.service.IService;
+import com.utils.PageUtils;
+import com.entity.GuanggaoxuqiuEntity;
+import java.util.List;
+import java.util.Map;
+import com.entity.vo.GuanggaoxuqiuVO;
+import org.apache.ibatis.annotations.Param;
+import com.entity.view.GuanggaoxuqiuView;
+
+
+/**
+ * 广告需求
+ *
+ */
+public interface GuanggaoxuqiuService extends IService<GuanggaoxuqiuEntity> {
+
+    PageUtils queryPage(Map<String, Object> params);
+
+    List<GuanggaoxuqiuVO> selectListVO(Wrapper<GuanggaoxuqiuEntity> wrapper);
+
+    GuanggaoxuqiuVO selectVO(@Param("ew") Wrapper<GuanggaoxuqiuEntity> wrapper);
+
+    List<GuanggaoxuqiuView> selectListView(Wrapper<GuanggaoxuqiuEntity> wrapper);
+
+    GuanggaoxuqiuView selectView(@Param("ew") Wrapper<GuanggaoxuqiuEntity> wrapper);
+
+    PageUtils queryPage(Map<String, Object> params,Wrapper<GuanggaoxuqiuEntity> wrapper);
+
+}
+
