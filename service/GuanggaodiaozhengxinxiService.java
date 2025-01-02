e9069668d574de946c452c7aa7d1116555eb7b5c
tangandrea <74780221+tangandrea@users.noreply.github.com>
2025-01-02 18:21:38 +0800

    Add files via upload

---
+++ service/GuanggaodiaozhengxinxiService.java
@@ -0,0 +1,33 @@
+package com.service;
+
+import com.baomidou.mybatisplus.mapper.Wrapper;
+import com.baomidou.mybatisplus.service.IService;
+import com.utils.PageUtils;
+import com.entity.GuanggaodiaozhengxinxiEntity;
+import java.util.List;
+import java.util.Map;
+import com.entity.vo.GuanggaodiaozhengxinxiVO;
+import org.apache.ibatis.annotations.Param;
+import com.entity.view.GuanggaodiaozhengxinxiView;
+
+
+/**
+ * 广告调整信息
+ *
+ */
+public interface GuanggaodiaozhengxinxiService extends IService<GuanggaodiaozhengxinxiEntity> {
+
+    PageUtils queryPage(Map<String, Object> params);
+
+    List<GuanggaodiaozhengxinxiVO> selectListVO(Wrapper<GuanggaodiaozhengxinxiEntity> wrapper);
+
+    GuanggaodiaozhengxinxiVO selectVO(@Param("ew") Wrapper<GuanggaodiaozhengxinxiEntity> wrapper);
+
+    List<GuanggaodiaozhengxinxiView> selectListView(Wrapper<GuanggaodiaozhengxinxiEntity> wrapper);
+
+    GuanggaodiaozhengxinxiView selectView(@Param("ew") Wrapper<GuanggaodiaozhengxinxiEntity> wrapper);
+
+    PageUtils queryPage(Map<String, Object> params,Wrapper<GuanggaodiaozhengxinxiEntity> wrapper);
+
+}
+
---
+++ service/GuanggaotoufangqudaoService.java
@@ -0,0 +1,33 @@
+package com.service;
+
+import com.baomidou.mybatisplus.mapper.Wrapper;
+import com.baomidou.mybatisplus.service.IService;
+import com.utils.PageUtils;
+import com.entity.GuanggaotoufangqudaoEntity;
+import java.util.List;
+import java.util.Map;
+import com.entity.vo.GuanggaotoufangqudaoVO;
+import org.apache.ibatis.annotations.Param;
+import com.entity.view.GuanggaotoufangqudaoView;
+
+
+/**
+ * 广告投放渠道
+ *
+ */
+public interface GuanggaotoufangqudaoService extends IService<GuanggaotoufangqudaoEntity> {
+
+    PageUtils queryPage(Map<String, Object> params);
+
+    List<GuanggaotoufangqudaoVO> selectListVO(Wrapper<GuanggaotoufangqudaoEntity> wrapper);
+
+    GuanggaotoufangqudaoVO selectVO(@Param("ew") Wrapper<GuanggaotoufangqudaoEntity> wrapper);
+
+    List<GuanggaotoufangqudaoView> selectListView(Wrapper<GuanggaotoufangqudaoEntity> wrapper);
+
+    GuanggaotoufangqudaoView selectView(@Param("ew") Wrapper<GuanggaotoufangqudaoEntity> wrapper);
+
+    PageUtils queryPage(Map<String, Object> params,Wrapper<GuanggaotoufangqudaoEntity> wrapper);
+
+}
+
